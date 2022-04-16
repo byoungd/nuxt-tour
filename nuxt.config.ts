089b523d8ae26f8aada85ff8191df7f7c3ee6c39
byoungd <pg98@vip.qq.com>
2022-04-16 16:07:00 +0800

    chore: use pnpm and add eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,41 @@
-import { defineNuxtConfig } from "nuxt3";
+import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
-    buildModules: [
-        // pinia plugin - https://pinia.esm.dev
-        "@pinia/nuxt",
-    ],
-    build: {
-        transpile: ['@heroicons/vue'],
-        postcss: {
-            postcssOptions: {
-                plugins: {
-                    tailwindcss: {},
-                    autoprefixer: {},
-                },
-            },
-        },
-    },
+  buildModules: [
+    // pinia plugin - https://pinia.esm.dev
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+  ],
+  vueuse: {
+    ssrHandlers: true,
+  },
+  build: {
+    transpile: ['@heroicons/vue'],
     postcss: {
+      postcssOptions: {
         plugins: {
           tailwindcss: {},
           autoprefixer: {},
         },
+      },
     },
-    vite: {
-        logLevel: "info",
-        optimizeDeps: {
-            include: [
-                '@headlessui/vue', '@heroicons/vue/solid', '@heroicons/vue/outline', 'vue', 'pinia'
-            ]
-        }
-    }
-});
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  vite: {
+    logLevel: 'info',
+    optimizeDeps: {
+      include: [
+        '@headlessui/vue',
+        '@heroicons/vue/solid',
+        '@heroicons/vue/outline',
+        'vue',
+        'pinia',
+      ],
+    },
+  },
+})
