f3a077fcf1f87b07ee8b1b6f082ab15b2b2e4583
byoungd <pg98@vip.qq.com>
2022-04-16 12:20:40 +0800

    feat: use tailwind template

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+import { defineNuxtConfig } from "nuxt3";
+
+export default defineNuxtConfig({
+    buildModules: [
+        // pinia plugin - https://pinia.esm.dev
+        "@pinia/nuxt",
+    ],
+    build: {
+        transpile: ['@heroicons/vue'],
+        postcss: {
+            postcssOptions: {
+                plugins: {
+                    tailwindcss: {},
+                    autoprefixer: {},
+                },
+            },
+        },
+    },
+    postcss: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+    },
+    vite: {
+        logLevel: "info",
+        optimizeDeps: {
+            include: [
+                '@headlessui/vue', '@heroicons/vue/solid', '@heroicons/vue/outline', 'vue', 'pinia'
+            ]
+        }
+    }
+});
